d4e7b88a891fc30fc6c097f3c3abc0a33776a8d2
Siddharth Kumar <siddarth306@gmail.com>
2019-10-08 15:37:32 -0700

    More kurt changes

--- feature_kurtosis.py
+++ feature_kurtosis.py
@@ -4,7 +4,6 @@ import math
 
 
 def moving_kurt(data):
-    # settings
     window_size = math.ceil(len(data[0]) / 5.0)
     overlap = math.ceil(window_size / 2.0)
     result = []
@@ -14,12 +13,9 @@ def moving_kurt(data):
         kurt_list = []
         while col < len(row_data)-window_size:
 
-            print(row_data[col:col+window_size])
             kurt = kurtosis(row_data[col:col+window_size])
-            col += window_size
-
-            col = col-overlap
             kurt_list.append(kurt)
+            col += window_size - overlap
 
         result.append(kurt_list)
     return result
