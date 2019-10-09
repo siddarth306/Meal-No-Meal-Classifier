949496d1e8eb095045a7ce100c8fcf7f3d3842a6
akshay33 <ashar204@asu.edu.com>
2019-10-08 19:48:06 -0700

    modified main

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
 	data = parse_and_interpolate(files[0])
 	fft_features = get_fft_features(data)
 	moving_kurt_features = moving_kurt(data)
-	#moving_avg_features = np.array(moving_avg(data))
+	moving_avg_features = np.array(moving_avg(data))
 
 	for index in range(1, len(files)):
 		data = parse_and_interpolate(files[index])
@@ -27,20 +27,20 @@ def main():
 		fft_features = np.concatenate((fft_features, get_fft_features(data)), axis=0)
 		#print(moving_avg_features.shape,"***************************")
 		#print(np.array(moving_avg(data)).shape,"-----------------------")
-		#moving_avg_features = np.concatenate((moving_avg_features, np.array(moving_avg(data))), axis=0)
+		moving_avg_features = np.concatenate((moving_avg_features, np.array(moving_avg(data))), axis=0)
 		moving_kurt_features = np.concatenate((moving_kurt_features, moving_kurt(data)), axis=0)
 
 
-	feature_mattrix = np.concatenate(( moving_kurt_features, fft_features), axis=1)
+	feature_mattrix = np.concatenate(( moving_avg_features, moving_kurt_features, fft_features), axis=1)
 	print(feature_mattrix.shape)
 
 	pca_matrix = performPCA(feature_mattrix)
 	print(pca_matrix.shape)
 	print(pca_matrix)
 
-	plot(pca_matrix[:,4])
+	# plot(pca_matrix[:,4])
 	# plot(fft_features)
-	# plot(moving_avg)
+	plot(moving_avg_features[:, 2])
 	# plot(moving_kurt)
 
 main()
