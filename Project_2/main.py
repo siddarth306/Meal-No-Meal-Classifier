80fe50a8bb5564c60c878db050d8bf54205d33a7
akshay33 <ashar204@asu.edu.com>
2019-11-07 15:25:54 -0700

    modified data

--- Project_2/main.py
+++ Project_2/main.py
@@ -18,8 +18,16 @@ def plot(data, title, color):
 
 
 def main():
-	files = ['CGMSeries/CGMSeriesLunchPat1.csv', 'CGMSeries/CGMSeriesLunchPat2.csv', 'CGMSeries/CGMSeriesLunchPat3.csv', 'CGMSeries/CGMSeriesLunchPat4.csv', 'CGMSeries/CGMSeriesLunchPat5.csv']
-	
+	files = [
+			'MealNoMealData/mealData1.csv', 'MealNoMealData/mealData2.csv', 
+			'MealNoMealData/mealData3.csv', 'MealNoMealData/mealData4.csv', 
+			'MealNoMealData/mealData5.csv', 'MealNoMealData/Nomeal1.csv',
+			'MealNoMealData/Nomeal2.csv', 'MealNoMealData/Nomeal3.csv',
+			'MealNoMealData/Nomeal4.csv', 'MealNoMealData/Nomeal5.csv'
+			]
+
+
+	#------------------- for meal data-----------------------------
 	data = parse_and_interpolate(files[0])
 	fft_features = get_fft_features(data)
 	#moving_kurt_features = moving_kurt(data)
@@ -27,7 +35,7 @@ def main():
 	moving_avg_features = np.array(moving_avg(data))
 	normal_skew_feature = normal_skew(data)
 
-	for index in range(1, len(files)):
+	for index in range(1, int(len(files)/2)):
 		data = parse_and_interpolate(files[index])
 	
 		fft_features = np.concatenate((fft_features, get_fft_features(data)), axis=0)
@@ -40,8 +48,9 @@ def main():
 	np.set_printoptions(suppress=True)
 	print(feature_mattrix)
 	print(feature_mattrix.shape)
+	
 
-	pca_matrix = performPCA(feature_mattrix)
+	pca_matrix1 = performPCA(feature_mattrix)
 
 	# -----------------plot moving average features--------------------------
 	# plot(moving_avg_features[:,0], "Moving Average 1", "blue")
