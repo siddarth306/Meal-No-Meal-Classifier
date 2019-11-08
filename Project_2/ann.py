3acd8140b3571880d81c93bced479c3e84d55812
akshay33 <ashar204@asu.edu.com>
2019-11-07 19:51:20 -0700

    created ann

---
+++ Project_2/ann.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+
+class ann:
+	
+	def __init__(self):
+		self.model = None
+
+	def train(self, data):
+
+		X_train = data[:,:5]
+		Y_train = data[:, 5]
+		self.model=keras.Sequential([
+			keras.layers.Dense(10,activation=tf.nn.sigmoid),
+			keras.layers.Dense(5,activation=tf.nn.sigmoid),
+			keras.layers.Dense(2,activation=tf.nn.softmax)
+			])
+
+		self.model.compile(optimizer='adamax', loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+		self.model.fit(X_train, Y_train, batch_size= 1500, epochs=1000)
+
+	def test(self, data):
+		X_test = data[:,:5]
+		Y_test = data[:, 5]
+		test_loss, test_acc = self.model.evaluate(X_test, Y_test)
