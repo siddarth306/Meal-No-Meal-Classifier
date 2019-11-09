ada2c1a92eef30515973359957a3953188cda5f9
Sanchit Pruthi <sanchitpruthi@gmail.com>
2019-11-08 23:13:52 -0700

    Adding DecisionTree For Janta

---
+++ Project_2/DecisionTree.py
@@ -0,0 +1,12 @@
+from sklearn import tree
+
+class DecisionTree():
+    def __init__(self):
+        __dr = None
+
+    def train(X,Y):
+        __dt = tree.DecisionTreeClassifier()
+        __dt.fit(X, Y)
+
+    def test(X):
+        return __gdt.predict(X)
