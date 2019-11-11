7b8bbd30a2bd8334ef83ca3e1070bb9eb563f62f
Siddharth Kumar <siddarth306@gmail.com>
2019-11-10 22:58:44 -0700

    Beautification of code

--- Project_2/main.py
+++ Project_2/main.py
@@ -1,30 +1,33 @@
-from utilities import parse_and_interpolate
+from utilities import *
 import numpy as np
-#from PCA_2 import performPCA
 import matplotlib.pyplot as plt
 import math
 from feature_selection import *
 import PCA as p
 import svm as s
 import ann as a
-import GaussianNaiveBayes as g
 import DecisionTree as d
 from sklearn.model_selection import KFold
+import adaBoost as ab
 
 
+# 4 classifiers used -> SVM, ANN, Ada Boost, Decision Trees
+
+# plot graph
 def plot(data, title, color):
 	plt.scatter(range(len(data)), data, c=color)
 	plt.title(title)
 	plt.xlabel("Time Series Data")
 	plt.ylabel("Feature Values")
 	plt.grid(True)
-	#plt.ylim(y_min, y_max)
 	plt.savefig(title)
 	plt.show()
 
 
+# get Feature mattrix for new test data
 def get_test_feature_matrix(filename, PCA):
-	data = parse_and_interpolate(filename)
+	#import pdb; pdb.set_trace()
+	data = parse_and_interpolate_test(filename)[0]
 	fft_features = get_fft_features(data)
 	entropy_feature = get_entropy(data)
 	moving_avg_features = moving_avg(data)
@@ -32,32 +35,39 @@ def get_test_feature_matrix(filename, PCA):
 
 	feature_mattrix = np.concatenate(( moving_avg_features, entropy_feature, fft_features, normal_skew_feature), axis=1)
 
-	#get PCA mattrix2 and add label
+	#get PCA mattrix
 	feature_mattrix = PCA.usePCA(feature_mattrix)
 	return feature_mattrix
 
 
-
+# get Feature mattrix for given data
 def get_feature_mattrix():
-	
-	files = [
-			'MealNoMealData/mealData1.csv', 'MealNoMealData/mealData2.csv', 
-			'MealNoMealData/mealData3.csv', 'MealNoMealData/mealData4.csv', 
-			'MealNoMealData/mealData5.csv', 'MealNoMealData/Nomeal1.csv',
-			'MealNoMealData/Nomeal2.csv', 'MealNoMealData/Nomeal3.csv',
-			'MealNoMealData/Nomeal4.csv', 'MealNoMealData/Nomeal5.csv'
-			]
+
+	meal_files =	[
+		   'MealNoMealData/mealData1.csv', 'MealNoMealData/mealData2.csv', 
+		   'MealNoMealData/mealData3.csv', 'MealNoMealData/mealData4.csv', 
+		   'MealNoMealData/mealData5.csv',
+		]
+	no_meal_files = [
+		   'MealNoMealData/Nomeal1.csv','MealNoMealData/Nomeal2.csv',
+		   'MealNoMealData/Nomeal3.csv','MealNoMealData/Nomeal4.csv',
+		   'MealNoMealData/Nomeal5.csv'
+		]
+
+	   
+	meal_data = parse_and_interpolate(meal_files)
+	no_meal_data = parse_and_interpolate(no_meal_files)
 
 	#------------------- for meal data-----------------------------
 	#----------------------label = 1-------------------------------
-	data = parse_and_interpolate(files[0])
+	data = meal_data[0]
 	fft_features = get_fft_features(data)
 	entropy_feature = get_entropy(data)
 	moving_avg_features = moving_avg(data)
 	normal_skew_feature = normal_skew(data)
 
-	for index in range(1, int(len(files)/2)):
-		data = parse_and_interpolate(files[index])
+	for index in range(1, len(meal_data)):
+		data = meal_data[index]
 	
 		fft_features = np.concatenate((fft_features, get_fft_features(data)), axis=0)
 		moving_avg_features = np.concatenate((moving_avg_features, moving_avg(data)), axis=0)
@@ -67,6 +77,7 @@ def get_feature_mattrix():
 	feature_mattrix = np.concatenate((moving_avg_features, entropy_feature, fft_features, normal_skew_feature), axis=1)
 	np.set_printoptions(suppress=True)
 
+	#get PCA mattrix2 and add label
 	PCA = p.cal_PCA()
 	pca_matrix1 = PCA.performPCA(feature_mattrix)
 	temp = np.ones((pca_matrix1.shape[0], 1))
@@ -75,14 +86,14 @@ def get_feature_mattrix():
 
 	#------------------- for no meal data-----------------------------
 	#-----------------------label = 0-------------------------------
-	data = parse_and_interpolate(files[int(len(files)/2)])
+	data = no_meal_data[0]
 	fft_features = get_fft_features(data)
 	entropy_feature = get_entropy(data)
 	moving_avg_features = moving_avg(data)
 	normal_skew_feature = normal_skew(data)
 
-	for index in range(int(len(files)/2)+1, len(files)):
-		data = parse_and_interpolate(files[index])
+	for index in range(1, len(no_meal_data)):
+		data = no_meal_data[index]
 	
 		fft_features = np.concatenate((fft_features, get_fft_features(data)), axis=0)
 		moving_avg_features = np.concatenate((moving_avg_features, moving_avg(data)), axis=0)
@@ -105,6 +116,7 @@ def get_feature_mattrix():
 	return feature_mattrix, PCA
 
 
+# Perfrom K-fold cross validation on all 4 models (k here is 4)
 def kFold(data):
 	kf = KFold(4,True,1)
 	evals1 = []
@@ -128,31 +140,31 @@ def kFold(data):
 	evals4 = np.array(evals4)
 	result_svm = np.mean(evals1, axis=0)
 	result_ann = np.mean(evals2, axis=0)
-	result_gnb = np.mean(evals3, axis=0)
+	result_ada = np.mean(evals3, axis=0)
 	result_dt = np.mean(evals4, axis=0)
 
 	resultlabel = ["Accuracy: ","Precision: ", "Recall: ", "F1 score: "]
 
 	print("K-Fold result")
 
-	print("SVM results:")
+	print("\nSVM results:")
 	for idx, i in enumerate(list(result_svm)):
 		print(resultlabel[idx],i)
 
-	print("ANN results:")
+	print("\nANN results:")
 	for idx, i in enumerate(result_ann):
 		print(resultlabel[idx],i)
 
-	print("Gaussian Naive Bayes results:")
-	for idx, i in enumerate(list(result_gnb)):
+	print("\nAda boost results:")
+	for idx, i in enumerate(list(result_ada)):
 		print(resultlabel[idx],i)
 
-	print("Decision Tree results:")
+	print("\nDecision Tree results:")
 	for idx, i in enumerate(result_dt):
 		print(resultlabel[idx],i)
 	return
 
-
+# train all the 4 models
 def training(x, y):
 	models = []
 	svm = s.SVM()
@@ -161,51 +173,67 @@ def training(x, y):
 	ann = a.Ann()
 	ann.train(x,y)
 	
-	gnb = g.GaussianNaiveBayes()
-	gnb.train(x,y)
+	ada = ab.AdaBoost()
+	ada.train(x,y)
 
 	dt = d.DecisionTree()
 	dt.train(x,y)
 
 	models.append(svm)
 	models.append(ann)
-	models.append(gnb)
+	models.append(ada)
 	models.append(dt)
 	return models
 
 
+# test all the 4 models
 def testing(models, x, y=None):
 	evals = []
 
 	svm = models[0]
 	ann = models[1]
-	gnb = models[2]
+	ada = models[2]
 	dt = models[3]
 
 	evals.append(svm.test(x, y))
 	evals.append(ann.test(x, y))
-	evals.append(gnb.test(x, y))
+	evals.append(ada.test(x, y))
 	evals.append(dt.test(x, y))
 	return tuple(evals)
 
 
-
-
 def main():
 	
-	feature_mattrix, PCA = get_feature_mattrix() # need the PCA object to fit testing
+	# get feature mattrix for the given dataset
+	feature_mattrix, PCA = get_feature_mattrix()
 
-	#kfold to evaluate model
+	# kfold to evaluate models
 	kFold(feature_mattrix)
 
+	# train on the given dataset
 	train_x, train_y = feature_mattrix[:, :-1], feature_mattrix[:, -1]
 	models = training(train_x, train_y)
 
-	filename = input("Input filename: ")
-	test_data = get_test_feature_matrix(filename, PCA) # need the PCA object to fit testing
+	filename = "1"
+	# input data file and get feature mattrix for new data
+	while True:
 
-	result = np.array(testing(models, test_data))
-	print(result)
+		filename = input("Input filename (Enter 0 to exit): ")
+		if filename == "0":
+			break
+		try:
+			test_data = get_test_feature_matrix([filename], PCA)
+		except Exception as e:
+			print("Error Cannot open file")
+			continue
+
+		# predict new data using learned models
+		result = testing(models, test_data)
+		result = np.array(result)
+		result_labels = ["\nSVM:\n","\nANN:\n", "\nAda Boost:\n", "\nDecision Tree:\n"]
+		for i in range(len(result_labels)):
+			print(result_labels[i], result[i])
+
 
 
 main()	
--- Project_2/utilities.py
+++ Project_2/utilities.py
@@ -1,21 +1,102 @@
 import numpy as np
 import math
 import pandas as pd
-from scipy.stats import kurtosis
+from sklearn.impute import SimpleImputer
+import sys
+from impyute.imputation.cs import fast_knn
+sys.setrecursionlimit(100000) #Increase the recursion limit of the OS
+from impyute.imputation.cs import mice
+import datawig
 
 
-def parse_and_interpolate(filename):
-    df = pd.read_csv(filename)
-    df_lists = df.values.tolist()
-    filled_df_list = []
-    for list_idx,each_list in enumerate(df_lists):
-        cleaned_data = pd.Series(each_list).interpolate(method='linear').tolist()
-        if not any([math.isnan(val) for val in cleaned_data]):
-            filled_df_list.append(cleaned_data)
+
+def parse_and_interpolate_test(filename_list):
+
+    all_dfs = []
+    len_list = [0]*len(filename_list)
    
-    filled_df = pd.DataFrame(filled_df_list, columns=df.columns)
+    for idx,i in enumerate(filename_list):
+        df = pd.read_csv(i)
+        df_lists = df.values.tolist()
+   
 
-    return np.array(filled_df)
+        all_dfs = all_dfs + df_lists
+        len_list[idx] = len(df_lists)
+    filled_df_list = []
+    if np.isnan(np.array(all_dfs)).any():
+        filled_df = mice(np.array(all_dfs))
+    else:
+        filled_df = np.array(all_dfs)
+
+    result = []
+    start = 0
+    for i in len_list:
+        result.append(filled_df[start:start+i])
+        start = i
+
+    return result
+
+
+
+
+def parse_and_interpolate(filename_list):
+
+    all_dfs = []
+    len_list = [0]*len(filename_list)
+   
+    for idx,i in enumerate(filename_list):
+        df = pd.read_csv(i)
+        df_lists = df.values.tolist()
+   
+
+        valid_df_list = []
+        for list_idx,each_list in enumerate(df_lists):
+            nanPercent = sum([1  if math.isnan(val) else 0 for val in each_list]) / len(each_list)
+            #nan_check = np.isnan(np.array(each_list)).all()
+            if nanPercent < 0.3:
+                valid_df_list.append(each_list)
+        all_dfs = all_dfs + valid_df_list
+        len_list[idx] = len(valid_df_list)
+    filled_df_list = []
+    if np.isnan(np.array(all_dfs)).any():
+        filled_df = mice(np.array(all_dfs))
+    else:
+        filled_df = np.array(all_dfs)
+
+    result = []
+    start = 0
+    for i in len_list:
+        result.append(filled_df[start:start+i])
+        start = i
+
+    return result
+
+
+#def parse_and_interpolate(filename):
+#    df = pd.read_csv(filename)
+#    df_lists = df.values.tolist()
+#    filled_df_list = []
+#    #import pdb;pdb.set_trace()
+#    for list_idx,each_list in enumerate(df_lists):
+#        #nanPercent = sum([1  if math.isnan(val) else 0 for val in each_list]) / len(each_list)
+#        if not all([math.isnan(val) for val in each_list]):
+#        #if nanPercent < 0.8:
+#            #cleaned_data = pd.Series(each_list).interpolate(method='linear').tolist()
+#            #imp_mean = SimpleImputer( strategy='most_frequent')
+#            #imp_mean.fit(each_list)
+#            #cleaned_data = imp_mean.transform(each_list)
+#            #if not np.any(np.isnan(cleaned_data)) and np.all(np.isfinite(cleaned_data)):
+#            filled_df_list.append(each_list)
+#   
+#    #imp_mean = SimpleImputer( strategy='most_frequent')
+#    #imp_mean.fit(filled_df_list)
+#    filled_df = mice(np.array(filled_df_list))
+#    #filled_df = fast_knn(np.array(df_lists), k=30)
+#    #cleaned_data = imp_mean.transform(filled_df_list)
+#    #filled_df = pd.DataFrame(cleaned_data, columns=df.columns)
+#
+#    return np.array(filled_df)
+
 
 def parse(filename):
     r = np.genfromtxt(filename, delimiter=',', names=True, case_sensitive=True, dtype=np.float64)
